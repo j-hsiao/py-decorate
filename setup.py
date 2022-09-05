b7240bb72f1cfa9c69ba297825246f31c9e67cfc
Hsiao <Jason.Hsiao@acer.com>
2022-09-05 10:09:50 +0100

    change names to use pep503 normalized names (. become -)

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 from jhsiao.namespace import make_ns, fdir
 make_ns('jhsiao', dir=fdir(__file__))
 setup(
-    name='jhsiao.decorate',
+    name='jhsiao-decorate',
     version='0.0.1',
     author='Jason Hsiao',
     author_email='oaishnosaj@gmail.com',
     description='Various decorators.',
     packages=['jhsiao.decorate'],
-    install_requires=['jhsiao.utils @ git+https://github.com/j-hsiao/py-utils.git']
+    install_requires=['jhsiao-utils @ git+https://github.com/j-hsiao/py-utils.git']
 )
